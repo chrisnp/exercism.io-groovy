ba06037fbd4e1efdbfdf88e1b783dbe82622732e
Invigo <invigo.devel@gmail.com>
2019-05-30 09:30:41 +0300

    pangram solution

---
+++ pangram/src/main/groovy/Pangram.groovy
@@ -0,0 +1,15 @@
+class Pangram {
+
+    static boolean isPangram(String sentence) {
+        
+        if (sentence != "") {
+
+            for (String letter : ('a'..'z')) {
+                if (!sentence.toLowerCase().contains(letter)) 
+                    return false
+            }
+            return true
+        }
+        return false
+    }
+}
