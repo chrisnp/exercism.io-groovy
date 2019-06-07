c24e76696be8456b2c9bb78d64b937fcd22cf6b1
Invigo <invigo.devel@gmail.com>
2019-06-07 08:12:03 +0300

    scrabble-score solution

---
+++ scrabble-score/src/main/groovy/ScrabbleScore.groovy
@@ -0,0 +1,17 @@
+class ScrabbleScore {
+
+    static scoreWord(String word) {
+        word
+        .trim()
+        .toUpperCase()
+        .collect {
+            (it =~ /[QZ]/)        ? 10 :
+            (it =~ /[JX]/)         ? 8 : 
+            (it == 'K')            ? 5 :
+            (it =~ /[FHVWY]/)      ? 4 :
+            (it =~ /[BCMP]/)       ? 3 :
+            (it =~ /[DG]/)         ? 2 :
+            (it =~ /[AEIOULNRST]/) ? 1 : 0 }
+        .sum(0)
+    }
+}
