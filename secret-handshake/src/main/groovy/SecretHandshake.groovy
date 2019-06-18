5714d55d985ede04f24a0d0b06ac7acb0699a3cb
Invigo <invigo.devel@gmail.com>
2019-06-18 20:23:35 +0300

    secret-handshake refactor

--- secret-handshake/src/main/groovy/SecretHandshake.groovy
+++ secret-handshake/src/main/groovy/SecretHandshake.groovy
@@ -1,17 +1,15 @@
 class SecretHandshake {
 
-    private static final signals = [
-            1: 'wink',
-            2: 'double blink',
-            4: 'close your eyes',
-            8: 'jump'
-    ]
-
     static List<String> commands(int number) {
-        
-        List<String> handshake = signals.findAll { (number & it.key) == it.key }
-                                        .collect { it.value }
 
-        (number & 16) == 16 ? handshake.reverse() : handshake
+        List<String> handshake = []
+        
+        if ((0x01 & number) != 0) handshake << 'wink'
+        if ((0x02 & number) != 0) handshake << 'double blink'
+        if ((0x04 & number) != 0) handshake << 'close your eyes'
+        if ((0x08 & number) != 0) handshake << 'jump'
+        if ((0x10 & number) != 0) Collections.reverse(handshake)
+        
+        handshake
     }
 }
