3e5bb3712889359a99920bf1f034c6bb5eb4924d
Invigo <invigo.devel@gmail.com>
2019-06-25 16:27:21 +0300

    matrix solution

---
+++ matrix/src/main/groovy/Matrix.groovy
@@ -0,0 +1,18 @@
+class Matrix {
+
+    private final int[][] matrix
+
+    Matrix(String asString) {
+        matrix = asString.split('\n')
+                         .collect { row -> row.split(' ')
+                                              .collect { c -> c as int }}
+    }
+
+    int[] row(int rowNumber) {
+        matrix[rowNumber]
+    }
+
+    int[] column(columnNumber) {
+        matrix.collect { row -> row[columnNumber] }
+    }
+}
