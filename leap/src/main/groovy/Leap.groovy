2334ed779f184e61ca44884cc3f1df6a963bf33e
Invigo <invigo.devel@gmail.com>
2019-06-29 15:50:53 +0300

    leap solution

---
+++ leap/src/main/groovy/Leap.groovy
@@ -0,0 +1,16 @@
+class Leap {
+
+    private final int year
+
+    Leap(int year) {
+
+        this.year = year
+    }
+
+    boolean divisibleBy(int x) { year % x == 0 }
+
+    boolean isLeapYear() {
+
+        divisibleBy(100) ? divisibleBy(400) : divisibleBy(4)
+    }
+}
