fd61e95401711385dae28e8e88f78e72aa50a879
Invigo <invigo.devel@gmail.com>
2019-07-01 15:45:58 +0300

    triangle solution

--- triangle/src/main/groovy/Triangle.groovy
+++ triangle/src/main/groovy/Triangle.groovy
@@ -1,15 +1,36 @@
 class Triangle {
 
+    private static boolean validTriangle(Number a, Number b, Number c) {
+
+        boolean triangleInequality = 
+            2 * [a, b, c].max() <= a + b + c 
+            
+        boolean degenerate = 
+            [a, b, c].any { s -> s <= 0 }
+        
+        triangleInequality && !degenerate
+    }
+
+    private static int uniqSideLengths(Number a, Number b, Number c) {
+
+        ([a, b, c] as Set).size()
+    }
+
     static boolean isEquilateral(Number a, Number b, Number c) {
-        throw new UnsupportedOperationException('isEquilateral method not implemented.')
+        
+        validTriangle(a, b, c) && 
+        uniqSideLengths(a, b, c) == 1
     }
 
     static boolean isIsosceles(Number a, Number b, Number c) {
-        throw new UnsupportedOperationException('isIsosceles method not implemented.')
+
+        validTriangle(a, b, c) && 
+        uniqSideLengths(a, b, c) <= 2
     }
 
     static boolean isScalene(Number a, Number b, Number c) {
-        throw new UnsupportedOperationException('isScalene method not implemented.')
+        
+        validTriangle(a, b, c) && 
+        uniqSideLengths(a, b, c) == 3
     }
-
 }
