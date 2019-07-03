ea846ec54471422119c0fefead776b87c69ffd4d
Invigo <invigo.devel@gmail.com>
2019-07-03 23:25:25 +0300

    isbn-verifier 2nd iteration

--- isbn-verifier/src/main/groovy/IsbnVerifier.groovy
+++ isbn-verifier/src/main/groovy/IsbnVerifier.groovy
@@ -2,12 +2,15 @@ class IsbnVerifier {
 
     static boolean isValid(String isbn) {
         
-        boolean wellFormed = (isbn ==~ /(\d)-?(\d{3})-?(\d{5})-?([0-9X])/)
-        boolean verified = isbn.findAll { (it as char).isLetterOrDigit() }
-                               .withIndex()
-                               .collect { c, i -> 
+        boolean wellFormed = isbn ==~ /(\d)-?(\d{3})-?(\d{5})-?([0-9X])/
+
+        if (!wellFormed) return false
+
+        boolean valid = isbn.findAll { (it as char).isLetterOrDigit() }
+                            .withIndex()
+                            .collect { c, i -> 
                                           c == 'X' ? 10 : (c as int) * (10 - i)}
-                               .sum() % 11 == 0
-        wellFormed && verified
+                            .sum() % 11 == 0
+        valid
     }
 }
