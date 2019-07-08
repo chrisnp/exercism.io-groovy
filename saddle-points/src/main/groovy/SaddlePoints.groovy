c2f10af45738ebc6e0af652484a3c657375c7353
Invigo <invigo.devel@gmail.com>
2019-07-08 23:38:40 +0300

    saddle-points refactor

--- saddle-points/src/main/groovy/SaddlePoints.groovy
+++ saddle-points/src/main/groovy/SaddlePoints.groovy
@@ -9,10 +9,12 @@ class SaddlePoints {
             int cols = matrix[0].size()
             (0 .. rows - 1).each { r -> 
                 (0 .. cols - 1).each { c ->
-                    if (matrix[r][c] == matrix[r].max() &&
-                        matrix[r][c] == matrix.collect { rs -> rs[c] }
-                                              .min())
-                            saddlePoints << [r, c]
+                    Closure saddle = {int row, int col -> 
+                        matrix[row][col] == matrix[row].max() &&
+                        matrix[row][col] == matrix.collect { rs -> rs[col] }
+                                                  .min()
+                    }
+                    if ( saddle(r, c) ) saddlePoints << [r, c]
                 }
             }
         }
