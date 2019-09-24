6637acdbc6557e22447da343e256f0d7bb812519
Invigo <invigo.devel@gmail.com>
2019-09-24 22:25:03 +0300

    retest anagram

---
+++ anagram/bin/main/Anagram.groovy
@@ -0,0 +1,26 @@
+class Anagram {
+
+    private final String word
+
+    Anagram(String word) {
+        this.word = word
+    }
+
+    private String normalForm(String s) {
+        s.toLowerCase().collect().sort()
+    }
+
+    private boolean duplicate(String candidate) {
+        word.equalsIgnoreCase(candidate)
+    }
+
+    private boolean anagram(String candidate) {
+        normalForm(word) == normalForm(candidate)
+    }
+ 
+    def find(List<String> candidates) {
+        candidates.findAll {
+            x -> anagram(x) && !duplicate(x)
+        }
+    }
+}
