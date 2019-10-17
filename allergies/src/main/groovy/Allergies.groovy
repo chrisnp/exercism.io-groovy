940eeb0d79cad9501c9a6a6bacfc9e61de811282
Invigo <invigo.devel@gmail.com>
2019-10-17 20:50:02 +0300

    refactor allergies

--- allergies/src/main/groovy/Allergies.groovy
+++ allergies/src/main/groovy/Allergies.groovy
@@ -1,7 +1,23 @@
 class Allergies {
 
+    private static final CAUSES = ['eggs', 'peanuts', 
+                                   'shellfish', 'strawberries',
+                                   'tomatoes', 'chocolate', 
+                                   'pollen', 'cats']
+
+    private final List<String> allergies
+
     Allergies(int score) {
-        
+        allergies = CAUSES.findAll { 
+            (score & (1 << CAUSES.indexOf(it))) != 0
+        }.collect { it }
     }
 
+    boolean allergicTo(String allergen) {
+        allergies.indexOf(allergen) != -1
+    }
+
+    List<String> list() {
+        allergies
+    }
 }
