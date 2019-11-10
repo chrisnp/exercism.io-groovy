5ca4d097fa1cb9cf6e205ec54c393a9931458d69
Invigo <invigo.devel@gmail.com>
2019-11-10 17:08:23 +0200

    all-your-base solution (1st iteration)

--- all-your-base/src/main/groovy/AllYourBase.groovy
+++ all-your-base/src/main/groovy/AllYourBase.groovy
@@ -1,10 +1,26 @@
 class AllYourBase {
 
-    AllYourBase(inputBase, digits) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+    private final Integer numInBase
+
+    AllYourBase(int inputBase, List<Integer> digits) {
+        if (inputBase < 2)
+            throw new ArithmeticException("invalid input base")
+        numInBase = digits.reverse().indexed().collect { i, d -> 
+            if (d < 0 || d >= inputBase) 
+                throw new ArithmeticException("invalid digit in representation")
+            d * inputBase ** i
+        }.sum(0) as int     
     }
 
     def rebase(outputBase) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+        if (outputBase < 2)
+            throw new ArithmeticException("invalid output base")
+        List<Integer> newRepresentation = []
+        int n = numInBase
+        while (n > 0) {
+            newRepresentation << n % outputBase
+            n /= outputBase
+        }
+        newRepresentation.reverse()?: [0]
     }
 }
