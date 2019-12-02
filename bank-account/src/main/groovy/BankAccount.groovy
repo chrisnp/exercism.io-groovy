271d56ca048687947af1cf293512fc790bb66abe
Invigo <invigo.devel@gmail.com>
2019-12-03 01:58:21 +0200

    bank-account refactor (3rd iteration)

--- bank-account/src/main/groovy/BankAccount.groovy
+++ bank-account/src/main/groovy/BankAccount.groovy
@@ -3,6 +3,16 @@ class BankAccount {
     private boolean open
     int balance
 
+    private void notClosed() {
+        if (!open)
+            throw new Exception("Account is closed")
+    }
+
+    private void notNegative(amount) {
+        if (amount < 0)
+            throw new Exception("Don't be silly")
+    }
+
     // You cannot do any operations before you open the account.
     // An account opens with a balance of 0
     // You can reopen an account
@@ -20,11 +30,9 @@ class BankAccount {
     // you cannot deposit into a closed account
     // you cannot deposit a negative amount 
     synchronized void deposit(int amount) {
-        if (!open) 
-            throw new Exception("Account is closed")
-        if (amount < 0) 
-            throw new Exception("Negative deposit")
-        balance += amount 
+        notClosed()
+        notNegative(amount)
+        balance += amount
     }
 
     // this should decrement the balance
@@ -32,20 +40,16 @@ class BankAccount {
     // you cannot withdraw a negative amount 
     // you cannot withdraw more than the balance
     synchronized void withdraw(int amount) {
-        if (!open) 
-            throw new Exception("Account is closed")
-        if (amount <= 0) 
-            throw new Exception("Can't withdraw less than zero")
+        notClosed()
+        notNegative(amount) 
         if (amount > balance) 
             throw new Exception("Amount requested exceeds balance")
         balance -= amount
-
     }
 
     // returns the current balance
     synchronized int getBalance() {
-        if (!open) 
-            throw new Exception("Account is closed")
-        return balance
+        notClosed()
+        balance
     }
 }
