50839617f68f787df97e02ca38fb1cb923d4f42b
Invigo <invigo.devel@gmail.com>
2020-11-16 06:31:26 +0200

    Update Raindrops.groovy

--- raindrops/src/main/groovy/Raindrops.groovy
+++ raindrops/src/main/groovy/Raindrops.groovy
@@ -1,8 +1,14 @@
 class Raindrops {
 
     def convert(num) {
-        [3: 'Pling', 5: 'Plang', 7: 'Plong']
-        .collect { drop -> num % drop.key == 0 ? drop.value : "" }
+        [3: 'Pling', 
+         5: 'Plang', 
+         7: 'Plong']
+        .collect { drop -> 
+                   num % drop.key == 0 ? 
+                         drop.value 
+                         : "" 
+                 }
         .join("") ?: "${num}"
     }   
 
