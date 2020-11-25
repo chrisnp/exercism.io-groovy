d68eaa939a2e97bd11d93db0539d988f8acae19c
Invigo <invigo.devel@gmail.com>
2020-11-25 23:29:27 +0200

    Update Isogram.groovy

--- isogram/src/main/groovy/Isogram.groovy
+++ isogram/src/main/groovy/Isogram.groovy
@@ -2,11 +2,14 @@ class Isogram {
 
     static boolean isIsogram(String phrase) {
 
-        def normalized = phrase.toLowerCase()
-                         .toCharArray()
-                         .findAll{ it.isLetterOrDigit() }
+        def normalized = 
+            phrase.toLowerCase()
+                  .toCharArray()
+                  .findAll{ it.isLetterOrDigit() }
         
-        return normalized.size() - normalized.unique().size() == 0
+        return normalized.size() - 
+               normalized.unique().size() 
+               == 0
 
     }
 
