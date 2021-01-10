7fb941ade5ef8fce983b68006eee9cafbc51fa4b
Invigo <invigo.devel@gmail.com>
2021-01-10 23:33:09 +0200

    Update SecretHandshake.groovy

--- secret-handshake/src/main/groovy/SecretHandshake.groovy
+++ secret-handshake/src/main/groovy/SecretHandshake.groovy
@@ -4,12 +4,16 @@ class SecretHandshake {
 
         List<String> handshake = []
         
-        if ((0x01 & number) != 0) handshake << 'wink'
-        if ((0x02 & number) != 0) handshake << 'double blink'
-        if ((0x04 & number) != 0) handshake << 'close your eyes'
-        if ((0x08 & number) != 0) handshake << 'jump'
-        if ((0x10 & number) != 0) Collections.reverse(handshake)
-        
+        if ((0x01 & number) != 0) 
+                    handshake << 'wink'
+        if ((0x02 & number) != 0) 
+                    handshake << 'double blink'
+        if ((0x04 & number) != 0) 
+                    handshake << 'close your eyes'
+        if ((0x08 & number) != 0) 
+                    handshake << 'jump'
+        if ((0x10 & number) != 0) 
+                    Collections.reverse(handshake)        
         handshake
     }
 }
