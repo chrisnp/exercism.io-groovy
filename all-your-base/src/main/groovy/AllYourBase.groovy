ecea6a3976376a1566f0c368b674a47e97c9f216
Invigo <invigo.devel@gmail.com>
2021-01-21 17:09:21 +0200

    Update AllYourBase.groovy

--- all-your-base/src/main/groovy/AllYourBase.groovy
+++ all-your-base/src/main/groovy/AllYourBase.groovy
@@ -4,17 +4,25 @@ class AllYourBase {
 
     AllYourBase(int inputBase, List<Integer> digits) {
         if (inputBase < 2)
-            throw new ArithmeticException("invalid input base")
-        numInBase = digits.reverse().indexed().collect { i, d -> 
-            if (d < 0 || d >= inputBase) 
-                throw new ArithmeticException("invalid digit in representation")
-            d * inputBase ** i
-        }.sum(0) as int     
+            throw new ArithmeticException(
+                "invalid input base"
+            )
+        numInBase = 
+            digits.reverse().indexed().collect { i, d -> 
+                if (d < 0 || d >= inputBase) 
+                    throw new ArithmeticException(
+                        "invalid digit in representation"
+                    )
+                d * inputBase ** i
+            }
+            .sum(0) as int     
     }
 
     def rebase(outputBase) {
         if (outputBase < 2)
-            throw new ArithmeticException("invalid output base")
+            throw new ArithmeticException(
+                "invalid output base"
+            )
         List<Integer> newRepresentation = []
         int n = numInBase
         while (n > 0) {
