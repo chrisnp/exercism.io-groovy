b1132f7dc54bac1b3a6bbd1ea3be7cf492454e56
Invigo <invigo.devel@gmail.com>
2021-01-22 23:14:47 +0200

    Test refactored phone-number

--- phone-number/src/main/groovy/PhoneNumber.groovy
+++ phone-number/src/main/groovy/PhoneNumber.groovy
@@ -1,29 +1,31 @@
 class PhoneNumber {
 
-    private static final NANP = ~/^1?([2-9]\d{2})([2-9]\d{2})(\d{4})$/
-
-    final String number
-    final String areaCode
-    final String xchange 
-    final String subscriber 
+    private static final NANP = 
+        ~/^1?([2-9]\d{2})([2-9]\d{2})(\d{4})$/
      
-    PhoneNumber(String input) {
+    static String clean(String input) {
+
+        String number
+        String areaCode
+        String xchange 
+        String subscriber 
 
         def matcher = 
             (input.replaceAll(/\D+/, '') =~ NANP)
 
         if (matcher) {
-            areaCode = matcher.group(1)
-            xchange = matcher.group(2)
-            subscriber = matcher.group(3)
-            number = "${areaCode}${xchange}${subscriber}"
+            areaCode = 
+                matcher.group(1)
+            xchange = 
+                matcher.group(2)
+            subscriber = 
+                matcher.group(3)
+            number = 
+                "${areaCode}${xchange}${subscriber}"
         }
         else {
-            number = "0000000000"
+            throw new Exception()
         }
-    }
-
-    String toString() {
-        "(${areaCode}) ${xchange}-${subscriber}"
+        number
     }
 }
