bf767559ae9510e4956f6d94f2a6db8e0a68ae61
Invigo <invigo.devel@gmail.com>
2021-02-13 23:38:20 +0200

    Reformat AtbashCipher.groovy

--- atbash-cipher/src/main/groovy/AtbashCipher.groovy
+++ atbash-cipher/src/main/groovy/AtbashCipher.groovy
@@ -1,14 +1,17 @@
 class AtbashCipher {
 
-    static final ciphermap = [('a'..'z'),('z'..'a')]
-                             .transpose()
-                             .collectEntries()
+    static final ciphermap = 
+        [('a'..'z'),('z'..'a')].transpose()
+                               .collectEntries()
 
     static String encode(phrase) {
         phrase.toLowerCase()
               .chars
               .findAll { Character.isLetterOrDigit(it) }
-              .collect { k -> ciphermap.find {it.key == k}?.value ?: k }
+              .collect { k -> ciphermap
+                              .find {
+                                   it.key == k}?.value ?: k 
+                               }
               .collate(5, true)
               .collect {it.join()}
               .join(" ")            
@@ -17,7 +20,10 @@ class AtbashCipher {
     static String decode(phrase) {
          phrase.chars
                .findAll { Character.isLetterOrDigit(it) }
-               .collect { v -> ciphermap.find {it.value == v}?.key ?: v }
+               .collect { v -> ciphermap
+                               .find {
+                                    it.value == v}?.key ?: v 
+                                }
                .join()   
     }
 }
