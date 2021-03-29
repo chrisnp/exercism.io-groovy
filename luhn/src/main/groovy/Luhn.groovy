2fe9205c27c5ffe89d46395dbee721a19a34ac01
Invigo <invigo.devel@gmail.com>
2021-03-29 23:53:01 +0300

    Luhn.groovy, 1st passing iteration

--- luhn/src/main/groovy/Luhn.groovy
+++ luhn/src/main/groovy/Luhn.groovy
@@ -2,23 +2,15 @@ class Luhn {
 
     static boolean valid(String value) {
 
-        if (value ==~ /[^0-9]|[^\s]*/) return false
-        
-        List<String> sanitized = 
-                    value.toList()
-                         .findAll { it ==~ /\d/}
-                         .reverse()
-        
-        if (sanitized.size() <= 1) return false
+        if (!(value.replaceAll(/ /, '') ==~ /\d{2,}/))
+            return false      
 
-        sanitized.collect { it as int}
-                 .indexed()
-                 .collect {i, x -> i % 2 != 0 ? 
-                                   2 * x      : 
-                                   x > 9      ? 
-                                   x - 9      :
-                                   x 
-                          }
-                 .sum() % 10 == 0
+        value.replaceAll(/ /, '')
+             .reverse()
+             .collect { it as int }
+             .indexed()
+             .collect { i, x -> i % 2 ? 2 * x : x } 
+             .collect { x -> x > 9 ? x - 9 : x }
+             .sum() % 10 == 0
     }
 }
