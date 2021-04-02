f37f697933247098c1e76eb4c59c4f81cc10cd95
Invigo <invigo.devel@gmail.com>
2021-04-02 23:47:14 +0300

    Update NthPrime.groovy

--- nth-prime/src/main/groovy/NthPrime.groovy
+++ nth-prime/src/main/groovy/NthPrime.groovy
@@ -1,7 +1,32 @@
 class NthPrime {
 
-    static nth(int n) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+    static int nth(int n) {
+        if (n < 1) 
+            throw new ArithmeticException()
+        
+        if (n == 1)
+            return 2
+        
+        int count = 1
+        
+        for (int candidate = 3; count <= n; candidate += 2) {
+            if (isPrime(candidate)) {
+                count += 1
+            }
+            if (count == n) {
+                return candidate
+            }
+        }
     }
 
+    private static boolean isPrime(int n) {
+        if (n <= 1) return false
+        if (n == 2) return true
+        for (int i = 3; i <= Math.sqrt(n).intValue(); i += 2) {
+            if (n % i == 0) {
+                return false
+            }
+        }
+        return true
+    }
 }
