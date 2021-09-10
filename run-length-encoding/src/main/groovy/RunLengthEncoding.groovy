636147fbdf7458a4de67762e0cedc0cb41663c01
Invigo <invigo.devel@gmail.com>
2021-09-10 13:57:05 +0300

    Update RunLengthEncoding.groovy

--- run-length-encoding/src/main/groovy/RunLengthEncoding.groovy
+++ run-length-encoding/src/main/groovy/RunLengthEncoding.groovy
@@ -2,15 +2,11 @@ class RunLengthEncoding {
 
     static String encode(String input) {
         input
-        .replaceAll(/(\D)\1+/) { 
-            "${it[0].length()}${it[1]}" 
-        }
+        .replaceAll(/(\D)\1+/) { "${it[0].length()}${it[1]}" }
     }
 
     static String decode(String input) {
         input
-        .replaceAll(/(\d+)(\D)/) { 
-            it[2] * (it[1] as int) 
-        }
+        .replaceAll(/(\d+)(\D)/) { it[2] * (it[1] as int) }
     }
 }
