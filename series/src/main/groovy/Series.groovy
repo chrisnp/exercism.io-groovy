098a328f50419084526b47c74d8e4c39dde97067
Invigo <invigo.devel@gmail.com>
2023-10-20 00:54:20 +0300

    Update Series.groovy

--- series/src/main/groovy/Series.groovy
+++ series/src/main/groovy/Series.groovy
@@ -1,15 +1,10 @@
 class Series {
-
     static List<String> slices(String series, int sliceLength) {
-
         if (sliceLength <= 0 || sliceLength > series.length()) 
             throw new ArithmeticException("invalid slice")
-
         List<String> subs = []
-
         for (i in 0..series.length() - sliceLength)
             subs << series.substring(i, i + sliceLength) 
-
         subs
     }
 }
