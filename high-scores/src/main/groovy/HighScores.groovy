7cd55b040622da023f5871ddc6a045e42f04a164
Invigo <invigo.devel@gmail.com>
2023-10-20 00:56:47 +0300

    Update HighScores.groovy

--- high-scores/src/main/groovy/HighScores.groovy
+++ high-scores/src/main/groovy/HighScores.groovy
@@ -1,22 +1,16 @@
 class HighScores {
-
     final List<Integer> scores
 
     HighScores(List<Integer> scores) {
         this.scores = scores
     }
-
     int latest() {
         scores.last()
     }
-
     int personalBest() {
         scores.max()
     }
-
     List<Integer> personalTopThree() {
-        scores.collect()
-              .sort() { a, b -> b.compareTo(a) }
-              .take(3)
+        scores.collect().sort() { a, b -> b.compareTo(a) }.take(3)
     }
 }
