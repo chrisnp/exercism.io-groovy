b50d7326c178f7ff7943eeb6300de4006b6fe905
Invigo <invigo.devel@gmail.com>
2023-11-10 22:15:28 +0200

    Refactor Darts

--- darts/src/main/groovy/Darts.groovy
+++ darts/src/main/groovy/Darts.groovy
@@ -1,11 +1,10 @@
 class Darts {
     static int score(x, y) {
+        def points = 0
         def sumSquares = x * x + y * y
-        sumSquares <= 1   
-        ? 10 : 
-        sumSquares <= 25  
-        ? 5 : 
-        sumSquares <= 100 
-        ? 1 : 0
+        if (sumSquares <= 100) points += 1 
+        if (sumSquares <= 25)  points += 4  
+        if (sumSquares <= 1)   points += 5
+        points
     }
 }
