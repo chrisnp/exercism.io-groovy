eb873ca058d4bf323451b27bf9ec78c1a8a59aa2
Invigo <invigo.devel@gmail.com>
2023-11-23 21:28:11 +0200

    Create QueenAttack.groovy

---
+++ queen-attack/src/main/groovy/QueenAttack.groovy
@@ -0,0 +1,8 @@
+class QueenAttack {
+
+    static canAttack(Queen q1, Queen q2) {
+        def dRank = (q1.getRank() - q2.getRank()).abs()
+        def dFile = (q1.getFile() - q2.getFile()).abs()
+        dRank * dFile == 0 || dRank / dFile == 1
+    }
+}
