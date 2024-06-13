d7211025635a47001fc05b39fd4cec0b41efb962
Invigo <invigo.devel@gmail.com>
2024-06-13 23:00:08 +0300

    Update PerfectNumbers.groovy

--- perfect-numbers/src/main/groovy/PerfectNumbers.groovy
+++ perfect-numbers/src/main/groovy/PerfectNumbers.groovy
@@ -1,14 +1,10 @@
 class PerfectNumbers {
     static Classification classify(int num) {
-        if (num <= 0) 
-            throw new ArithmeticException("not a natural number")
-        
+        if (num <= 0) throw new ArithmeticException("not a natural number")
         int aliquot = (num > 1) 
-                      ? (1 .. Math.floor(num / 2))
-                        .findAll { n -> num % n == 0}
-                        .sum(0) as int
+                      ? (1 .. Math.floor(num / 2)).findAll { n -> num % n == 0}
+                                                  .sum(0) as int
                       : 0
-        
         switch (aliquot) {
             case { it == num }:
                 return Classification.PERFECT
