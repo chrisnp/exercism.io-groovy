528988491e5ffabd08d85d8fba0b20391c2fe428
Invigo <invigo.devel@gmail.com>
2024-06-24 16:31:57 +0300

    Sieve - passing all tests

--- sieve/src/main/groovy/Sieve.groovy
+++ sieve/src/main/groovy/Sieve.groovy
@@ -1,7 +1,17 @@
 class Sieve {
-
     static primes(Integer limit) {
-        throw new UnsupportedOperationException('primes method not implemented.')
+        limit += 1
+        def sieve = (1..limit).collect { true }
+        sieve[0] = false
+        for (int n = 2; n ** 2 <= limit; n++) 
+            if (sieve[n]) 
+                for (int m = n ** 2; m <= limit; m += n)
+                    sieve[m] = false
+        def primes = []
+        if (limit > 2) primes << 2
+        for (int candidate = 3; candidate <= limit; candidate += 2)
+            if (sieve[candidate])
+                primes << candidate
+        primes
     }
-
 }
