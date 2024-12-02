d5cf1b3c5855446e75aab8288cec50842af80237
Invigo <invigo.devel@gmail.com>
2024-12-02 13:33:46 +0200

    Refactor pascals-triangle

--- pascals-triangle/src/main/groovy/PascalsTriangle.groovy
+++ pascals-triangle/src/main/groovy/PascalsTriangle.groovy
@@ -4,17 +4,11 @@ class PascalsTriangle {
 
     static List<List<Integer>> rows(count) {
         def factorial = { 
-            IntStream.range(1, it + 1).reduce(1, (acc, x) -> acc * x)
-        }
-
+            IntStream.range(1, it + 1).reduce(1, (acc, x) -> acc * x) }
         def binomial = { i = j, j = 0 ->
-            factorial(i).intdiv(factorial(j) * factorial(i - j))
-        }
-
-        def row = { r -> 
-            IntStream.range(0, r + 1).map(x -> binomial(r, x)).toList()
-        }
-        
+            factorial(i).intdiv(factorial(j) * factorial(i - j)) }
+        def row = { 
+            IntStream.range(0, it + 1).map(x -> binomial(it, x)).toList() }
         IntStream.range(0, count).mapToObj(n -> row(n)).toList()
     }
 }
