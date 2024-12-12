f52e57642837f6db3fb483d70b16bee2386f053d
Invigo <invigo.devel@gmail.com>
2024-12-12 23:57:51 +0200

    Reformat Accumulate

--- accumulate/src/main/groovy/Accumulate.groovy
+++ accumulate/src/main/groovy/Accumulate.groovy
@@ -1,10 +1,7 @@
 class Accumulate {
 
     static accumulate(Collection coll, Closure func) {
-
-        if (!coll) 
-            []
-        else 
-            [func(coll.head()), *accumulate(coll.tail(), func)]
+        if (!coll) return [] 
+        [func(coll.head()), *accumulate(coll.tail(), func)]
     }
 }
