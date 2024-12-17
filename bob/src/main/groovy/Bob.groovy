8259c7eea93af9d99e7faa807dff1fa4dcfd10bd
Invigo <invigo.devel@gmail.com>
2024-12-17 22:50:32 +0200

    Refactor bob

--- bob/src/main/groovy/Bob.groovy
+++ bob/src/main/groovy/Bob.groovy
@@ -1,21 +1,13 @@
 class Bob {
-
-    private static boolean silence(String input) {
-        input.trim() == ''
-    }
-
-    private static boolean yell(String input) {
-        input = input.trim()
-        input == input.toUpperCase() 
-        ? input != input.toLowerCase()   
-        : false
-    }
-
-    private static boolean question(String input) {
-        input.trim().endsWith('?')
-    }
-
+    
     public static response(String input) {
+        def silence = { it.trim() == '' }
+        def yell = { 
+            it.trim() == it.trim().toUpperCase() 
+            ? it.trim() != it.trim().toLowerCase()
+            : false 
+        }
+        def question = { it.trim().endsWith('?') }
         if (silence(input)) 
             return "Fine. Be that way!"
         if (yell(input) && question(input))
